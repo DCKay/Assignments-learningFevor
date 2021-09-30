9ff3ece3951a615fe32daff23bf4af0bdca3c374
Kay <Kay>
2021-09-30 14:36:37 +0200

    Euler proj

---
+++ Euler project/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal = 100000;
+            int som = 0;
+            for (int i = 0; i <= getal; i++)
+            {
+                if (i <= 0)
+                {
+                    //skip
+                    continue;
+                }
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    Console.WriteLine(i);
+                    som += i;
+                    if (som >= 234168)
+                    {
+                        // Force end
+                        break;
+                    }
+                }
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Som: {som}");
+            Console.ReadLine();
+        }
+    }
+}
