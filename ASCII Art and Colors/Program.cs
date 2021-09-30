af830294e4bb30c7d645fa9cc1a9106a227f4863
Kay <Kay>
2021-09-30 12:59:47 +0200

    Update

---
+++ ASCII Art and Colors/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCII_Art_and_Colors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("/$$   /$$");
+            Console.WriteLine("| $$  /$$/");
+            Console.Write("| $$ /$$/");
+            Console.ResetColor();
+            Console.Write("   /$$$$$$  /$$   /$$\n");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("| $$$$$/");
+            Console.ResetColor();
+            Console.Write("   |____  $$| $$  | $$\n");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("| $$  $$");
+            Console.ResetColor();
+            Console.Write("    /$$$$$$$| $$  | $$\n");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("| $$\\  $$");
+            Console.ResetColor();
+            Console.Write("  /$$__  $$| $$  | $$\n");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("| $$ \\  $$");
+            Console.ResetColor();
+            Console.Write("|  $$$$$$$|  $$$$$$$\n");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("| __/ \\__/");
+            Console.ResetColor();
+            Console.Write(" \\_______/ \\____  $$\n");
+            Console.WriteLine("                     /$$  | $$");
+            Console.WriteLine("                    |  $$$$$$/");
+            Console.WriteLine("                     \\______/");
+            Console.ReadLine();
+
+
+        }
+    }
+}
